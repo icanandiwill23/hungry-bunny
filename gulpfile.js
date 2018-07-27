d02a7fad978c1f0dd2242bab23188473d8c0a5f5
Anthony <ajvergara2326@gmail.com>
2018-07-27 22:58:07 +0300

    Added PostCSS and Browser Sync Dev Dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,39 @@
 const gulp = require("gulp");
-      watch = require("gulp-watch");
+      watch = require("gulp-watch"),
+      postcss = require("gulp-postcss"),
+      cssVars = require("postcss-simple-vars"),
+      cssImport = require("postcss-import"),
+      cssNested = require("postcss-nested"),
+      autoprefixer = require("autoprefixer"),
+      browserSync = require("browser-sync").create();
+
+gulp.task("styles", function(){
+  return gulp.src("./app/assets/styles/styles.css")
+    .pipe(postcss([cssImport, autoprefixer, cssVars, cssNested]))
+    .on("error", function(err){
+      console.log(err.toString());
+      this.emit("end");
+    })
+    .pipe(gulp.dest("./app/temp/styles"));
+});
 
 gulp.task("watch", function(){
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch("./app/assets/styles/**/*.css", function(){
-    console.log("CSS has been changed");
+    gulp.start("cssInject");
   });
 
   watch("./app/*.html", function(){
-    console.log("HTML has been changed");
+    browserSync.reload();
   });
+});
+
+gulp.task("cssInject", ["styles"], function(){
+  return gulp.src("./app/temp/styles/styles.css")
+    .pipe(browserSync.stream());
 });
