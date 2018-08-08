560d792a1a3c3c080d56a6822da4f4463392e3a0
Anthony <ajvergara2326@gmail.com>
2018-08-08 20:33:22 +0300

    Completed Mobile Menu Icon

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,7 +4,9 @@ import slick from "slick-carousel";
 import OnLoad from "./modules/OnLoad";
 import WayPoints from "./modules/WayPoints";
 import Slider from "./modules/Slider";
+import MobileMenu from "./modules/MobileMenu";
 
 var onLoad = new OnLoad();
 var slider = new Slider();
+var mobileMenu = new MobileMenu();
 var cardsWayPoints = new WayPoints($(".cards"), "70%");
